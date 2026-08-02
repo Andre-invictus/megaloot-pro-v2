98a7cc86f1f50126a0796995646147fe97cf12af
Andre-invictus <unitecandre@hotmail.com>
2026-08-01 23:26:54 -0300

    Update Supabase anonymous key in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // Nunca coloque service_role, sb_secret_, senha do banco ou chave DVAPI aqui.
 
 window.MEGALOOT_SUPABASE_URL = "https://jusjftldnhdgrnexmpbx.supabase.co";
-window.MEGALOOT_SUPABASE_ANON_KEY = "sb_publishable_zb5UFPc6-FuknT1qbw2Yng_6uxl22y3";
+window.MEGALOOT_SUPABASE_ANON_KEY = "sb_publishable_dkJGzTE1tjavBB95O71wVw_TR-4fLhS";
 window.MEGALOOT_DVAPI_FUNCTION = "dvapi-proxy";
